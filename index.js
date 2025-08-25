49a62cdda8e044626af4620e11f0edad889585a3
manishyadav07s <ghanghrikura@gmail.com>
2025-08-25 16:54:19 +0530

    done

--- index.js
+++ index.js
@@ -11,6 +11,8 @@
  app.use(cors());
  app.use(express.json());
 
+  app.use("/api/auth", authRoutes);
+  app.use("/api/file", fileRoutes);
 
  const PORT = process.env.PORT || 5000;
  app.listen(PORT, () => console.log(` Server running on ${PORT}`));
