59a402d375a9d8daab1c6467e95a0b4698262448
manishyadav07s <ghanghrikura@gmail.com>
2025-08-25 16:55:00 +0530

    done

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
- import express from "express";
- import dotenv from "dotenv";
- import cors from "cors";
- import connectDB from "./config/db.js";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import fileRoutes from "./routes/fileRoutes.js";
 
+dotenv.config();
+connectDB();
 
- dotenv.config();
- connectDB();
+const app = express();
+app.use(cors());
+app.use(express.json());
 
- const app = express();
- app.use(cors());
- app.use(express.json());
+app.use("/api/auth", authRoutes);
+app.use("/api/file", fileRoutes);
 
-  app.use("/api/auth", authRoutes);
-  app.use("/api/file", fileRoutes);
-
- const PORT = process.env.PORT || 5000;
- app.listen(PORT, () => console.log(` Server running on ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
